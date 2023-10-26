5ea86ed703eabd56c54195790fb944b0df599a06
Alan Noruzbaev <alan.noruzbaev@gmail.com>
2023-10-26 16:25:48 +0600

    feat: Implemented delete task functionality

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,41 @@ import { useState } from 'react';
 import InputForm from './components/AddTaskForm.tsx';
 import Task from './components/Task.tsx';
 
+interface TaskItem {
+  id: string;
+  text: string;
+}
+
 const App = () => {
+  const [tasks, setTasks] = useState<TaskItem[]>([]);
 
-  const [tasks, setTasks]=useState<string[]>([])
+  const generateRndNumber = (): string => {
+    const rndNumber = Math.floor(Math.random() * 1000);
+    return String(rndNumber);
+  };
 
-  const addTask=(text:string)=>{
-    setTasks([...tasks, text])
-  }
+  const addTask = (text: string) => {
+    const id = generateRndNumber();
+    const task = tasks.find((task) => task.id === id);
+
+    if (task) {
+      addTask(text);
+    } else {
+      setTasks([...tasks, { id: id, text: text }]);
+    }
+  };
+
+  const deleteTask = (id: string) => {
+    const updatedTasks = tasks.filter((task) => task.id !== id);
+    setTasks(updatedTasks);
+  };
 
   return (
     <div className="container">
-      <InputForm onSubmit={addTask}/>
+      <InputForm onSubmit={addTask} />
       <div className="row">
-        {tasks.map((task,index)=>{
-          return <Task key={index} text={task}/>
+        {tasks.map((task) => {
+          return <Task key={task.id} id={task.id} text={task.text} onDelete={deleteTask} />;
         })}
       </div>
     </div>
--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 interface TaskProps {
   text: string;
+  id: string
+  onDelete: (id: string)=> void
 }
 
-const Task: React.FC<TaskProps> = ({text})=>{
+const Task: React.FC<TaskProps> = ({id, text, onDelete})=>{
+  const deleteTask = () => {
+    onDelete(id);
+  };
   return(
-    <div className="card">
+    <div className="card mb-3">
       <div className="card-body d-flex align-items-center">
         {text}
-        <button className="btn btn-danger ms-auto" type="button">Delete</button>
+        <button className="btn btn-danger ms-auto" type="button" onClick={deleteTask}>Delete</button>
       </div>
     </div>
   )
